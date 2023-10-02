494e57f1b13d22db789e53a723638498142b8827
hatemBay <bayoudhhatem5@gmail.com>
2023-10-02 13:37:20 +0100

    Add users resource and fields

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,18 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
-      definitions: {
-        path: join(process.cwd(), 'src/graphql.ts'),
-      },
+      driver: ApolloDriver,
+      // resolvers: { DateTime: Date },
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/graphql.ts
@@ -0,0 +1,49 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export enum Role {
+    ADMIN = "ADMIN",
+    USER = "USER",
+    GUEST = "GUEST"
+}
+
+export class CreateUserInput {
+    exampleField?: Nullable<number>;
+}
+
+export class UpdateUserInput {
+    id: number;
+}
+
+export class User {
+    id?: Nullable<string>;
+    username: string;
+    email: string;
+    password: string;
+    name?: Nullable<string>;
+    role: Role;
+    is_u_18?: Nullable<boolean>;
+}
+
+export abstract class IQuery {
+    abstract users(): Nullable<User>[] | Promise<Nullable<User>[]>;
+
+    abstract user(id: number): Nullable<User> | Promise<Nullable<User>>;
+}
+
+export abstract class IMutation {
+    abstract createUser(createUserInput: CreateUserInput): User | Promise<User>;
+
+    abstract updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
+
+    abstract removeUser(id: number): Nullable<User> | Promise<Nullable<User>>;
+}
+
+type Nullable<T> = T | null;
