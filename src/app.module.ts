cae46ceef5813d6695bc8af6672d3e26da9c5a1f
hatemBay <bayoudhhatem5@gmail.com>
2023-10-02 16:10:26 +0100

    Add prisma schema and migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLISODateTime, GraphQLModule } from '@nestjs/graphql';
+import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { UsersModule } from './users/users.module';
-// import { GraphQLDateTime } from 'graphql-iso-date';
+import { GraphQLDateTime } from 'graphql-iso-date';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       typePaths: ['./**/*.graphql'],
       driver: ApolloDriver,
-      resolvers: { DateTime: GraphQLISODateTime },
+      resolvers: { DateTime: GraphQLDateTime },
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
