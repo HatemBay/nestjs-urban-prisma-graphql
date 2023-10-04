f4e2983e1319db7940774aa29f5bf755743f8829
hatemBay <bayoudhhatem5@gmail.com>
2023-10-04 14:16:41 +0100

    Add passport local strategy authentication and guard control

--- src/@generated/user/user-where.input.ts
+++ src/@generated/user/user-where.input.ts
@@ -9,40 +9,39 @@ import { DateTimeFilter } from '../prisma/date-time-filter.input';
 
 @InputType()
 export class UserWhereInput {
+  @Field(() => [UserWhereInput], { nullable: true })
+  AND?: Array<UserWhereInput>;
 
-    @Field(() => [UserWhereInput], {nullable:true})
-    AND?: Array<UserWhereInput>;
+  @Field(() => [UserWhereInput], { nullable: true })
+  OR?: Array<UserWhereInput>;
 
-    @Field(() => [UserWhereInput], {nullable:true})
-    OR?: Array<UserWhereInput>;
+  @Field(() => [UserWhereInput], { nullable: true })
+  NOT?: Array<UserWhereInput>;
 
-    @Field(() => [UserWhereInput], {nullable:true})
-    NOT?: Array<UserWhereInput>;
+  @Field(() => IntFilter, { nullable: true })
+  id?: IntFilter;
 
-    @Field(() => IntFilter, {nullable:true})
-    id?: IntFilter;
+  @Field(() => StringFilter, { nullable: true })
+  username?: StringFilter;
 
-    @Field(() => StringFilter, {nullable:true})
-    username?: StringFilter;
+  @Field(() => StringFilter, { nullable: true })
+  email?: StringFilter;
 
-    @Field(() => StringFilter, {nullable:true})
-    email?: StringFilter;
+  @Field(() => StringFilter, { nullable: true })
+  password?: StringFilter;
 
-    @Field(() => StringFilter, {nullable:true})
-    password?: StringFilter;
+  @Field(() => StringNullableFilter, { nullable: true })
+  name?: StringNullableFilter;
 
-    @Field(() => StringNullableFilter, {nullable:true})
-    name?: StringNullableFilter;
+  @Field(() => EnumRoleFilter, { nullable: true })
+  role?: EnumRoleFilter;
 
-    @Field(() => EnumRoleFilter, {nullable:true})
-    role?: EnumRoleFilter;
+  @Field(() => BoolFilter, { nullable: true })
+  is_u_18?: BoolFilter;
 
-    @Field(() => BoolFilter, {nullable:true})
-    is_u_18?: BoolFilter;
+  @Field(() => DateTimeFilter, { nullable: true })
+  created_at?: DateTimeFilter;
 
-    @Field(() => DateTimeFilter, {nullable:true})
-    created_at?: DateTimeFilter;
-
-    @Field(() => DateTimeFilter, {nullable:true})
-    updated_at?: DateTimeFilter;
+  @Field(() => DateTimeFilter, { nullable: true })
+  updated_at?: DateTimeFilter;
 }
