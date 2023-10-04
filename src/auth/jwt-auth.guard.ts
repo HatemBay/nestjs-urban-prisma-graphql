0dd7db1d16ecf895d69dd76f645e73dfd025793b
hatemBay <bayoudhhatem5@gmail.com>
2023-10-04 19:24:33 +0100

    Add possibility to skip authentication with SkipAuth decorator metadata

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,11 +1,39 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
+import { Injectable, ExecutionContext, CanActivate } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  getRequest(context: ExecutionContext) {
+export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  public getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req;
+    const request = ctx.getContext().req;
+
+    return request;
+  }
+
+  // Handle request here works instead of canActivate because it will return an error
+  // if there's no authorization header in the request so the metadata should be handled
+  // before checking on the headers
+  handleRequest<TUser = any>(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status?: any,
+  ): TUser {
+    // const ctx = GqlExecutionContext.create(context);
+
+    const skipAuth =
+      this.reflector.get<boolean>('skipAuth', context.getHandler()) || false;
+
+    if (skipAuth) {
+      return null;
+    }
+    return super.handleRequest(err, user, info, context, status);
   }
 }
