26cbc202e9e93fd4f85aa366d1d1ffe4633477f3
hatemBay <bayoudhhatem5@gmail.com>
2023-10-09 17:28:25 +0100

    Add throttler for Rate Limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,8 +28,19 @@ import { ThrottlerModule } from '@nestjs/throttler';
     ConfigModule.forRoot(),
     ThrottlerModule.forRoot([
       {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
         ttl: 60000,
-        limit: 10,
+        limit: 100,
       },
     ]),
     UsersModule,
