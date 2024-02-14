4785ac65928f8eb864738b330a8ab1f27552dcad
HatemBay <bayoudhhatem5@gmail.com>
2024-02-14 15:42:00 +0100

    Increase limit for stored data to 10mb with raw body

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+  });
 
   const configService = app.get(ConfigService);
+
+  app.useBodyParser('json', { limit: '10mb' });
 
   app.enableCors();
 
